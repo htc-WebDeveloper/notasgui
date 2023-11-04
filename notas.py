fe7665655eed94b0643c8fb065dbfaad94e944df
HectoJoseDeFreitasRico <hjfnogueira@gmail.com>
2023-11-04 09:30:24 +0100

    dar de alta estilos

--- notas.py
+++ notas.py
@@ -1,13 +1,18 @@
 import tkinter as tk                        #importo la libreria de GUI
 from tkinter import tkinter                 #importo la nueva libreria ttk     
 
+#######################CREACION DE LA VENTANA PRINCIPAL Y ESTILO DE LA VENTANA##################
+
 raiz = tk.Tk()                              #creo una interfaz grafica de usuario
 raiz.title("Notas v0.01")                   #especifico el titulo de la ventana
 raiz.geometry('300x300+50+50')              #geometria de la ventana y margen con la pantalla
 raiz.attributes("-topmost",True)            #siempre encima del resto de las ventanas
 raiz.attributes("-alpha",0.9)               #añado un efecto de transparencia
 raiz.resizable(0,0)                         #impido que el usuario pueda redimensionar la ventana
+estilo = ttk.Style()                        #añado soporte para estilos
+estilo.theme_use('classic')                 #selecciono el estilo clasico de aplicaciones
 
+#########################INTENTO INTRODUCIR ANTIALIAS EN WINDOWS Y LANZO EL BUCLE###############
 
 try:                                        #intento ejecutar
     from ctypes import windll               #importo la libreria especifica de GUI de Windows
